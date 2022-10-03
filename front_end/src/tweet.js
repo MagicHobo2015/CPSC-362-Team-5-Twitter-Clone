3505da3970d474f084983660119e98a930dd94cd
MagicHobo2015 <joshua.land6@gmail.com>
2022-10-03 14:30:15 -0700

    got a prototype version of a tweet, its not connected to anything tho

---
+++ front_end/src/tweet.js
@@ -0,0 +1,12 @@
+
+/*
+// this is needed for all react components
+import React from 'react';
+// this imports the styling
+// import './index.css';
+
+function Tweet() {
+	return <div classname="Tweet">Tweet</div>
+}
+export default Tweet
+*/
