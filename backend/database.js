ab1aea95a0f67e40001baa8db7f707e360a0229f
MagicHobo2015 <joshua.land6@gmail.com>
2022-10-06 15:26:46 -0700

    recovering from the pop

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,11 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
+import { ContactlessOutlined } from '@material-ui/icons';
 // this reads the .env file 
 dotenv.config();
 
+// this will contain all the info node needs to connect to the database.
+// it will be used every time you interact with it.
 const pool = mysql.createPool({
 	host: process.env.MYSQL_HOST,
 	user: process.env.MYSQL_USER,
@@ -10,5 +13,25 @@ const pool = mysql.createPool({
 	database: process.env.MYSQL_DATABASE
 }).promise()
 
-const results = await pool.query("SELECT * FROM users");
-console.log(results[0]);
+// this will just return one row of info
+async function getTweet(id) {
+	const [row] = await pool.query(`
+	SELECT * FROM users 
+	WHERE id = ?`, [id])
+	return row[0]
+}
+
+// this adds a tweet to the database, needs to be adjusted to what the tweet model actually contains
+async function createTweet( name, email ) {
+	const [result] = await pool.query(`
+	INSERT INTO users (name, email) 
+	VALUES ( ?, ?)`, [name, email])
+}
+
+// get the tweet
+const idunno = getTweet(6);
+// idunno returns a promise so you call the then method
+idunno.then(response => {console.log(response)});
+
+
+
