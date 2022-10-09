f810d44203d481e3acf516ac2bc4c2d77aa0dc82
Takkong <38085027+Takkong@users.noreply.github.com>
2022-10-08 21:34:34 -0700

    Class for the tweet input with name and text

---
+++ front_end/src/TweetInput.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class TweetInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {aName: '', tweet: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        alert(this.state.aName + ' tweeted ' + this.state.tweet);
+        event.preventDefault();
+    }
+
+    render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+            <label>
+                Name:
+                <input name="aName" type="text" value={this.state.aName} onChange={this.handleChange} />
+            </label>
+            <br />
+            <label>
+                Tweet:
+                <textarea name="tweet" value={this.state.tweet} onChange={this.handleChange} />
+            </label>
+            <br />
+            <input type="submit" value="Tweet" />
+        </form>
+        );
+    }
+}
+
+export default TweetInput;
